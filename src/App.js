b2c9a25cf7705754321a732fe615e1d9281ac32b
miss6muffet <65518042+miss6muffet@users.noreply.github.com>
2020-09-12 17:20:32 +0200

    Source

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
 import React from "react";
-import CurrentWeather from "./CurrentWeather";
 import Forecast from "./Forecast";
 import ForecastPreview from "./ForecastPreview";
 import Search from "./Search";
 import Source from "./Source";
 import Weather from "./Weather";
+import WeatherConversion from "./WeatherConversion";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <Search />
-      <CurrentWeather />
       <Weather />
+      <WeatherConversion />
       <br />
       <Forecast />
       <ForecastPreview />
--- src/CurrentWeather.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-import "./CurrentWeather.css";
-
-export default function CurrentWeather() {
-  return <h1>36°C</h1>;
-}
