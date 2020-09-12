d44aef30a14e98145ad0b3d1232789c177924ee2
miss6muffet <65518042+miss6muffet@users.noreply.github.com>
2020-09-12 17:02:07 +0200

    Create WeatherConversion.js

---
+++ src/WeatherConversion.js
@@ -0,0 +1,35 @@
+import React from "react";
+
+export default function WeatherConversion(props) {
+  if (props.units === "imperial") {
+    return (
+      <div className="WeatherConversion">
+        <span className="temperature">{Math.round(props.temperature)}</span>
+        <span className="unit">°F</span>
+        <span className="unit"> | </span>
+        <span
+          className="unit convert"
+          onClick={() => props.setUnits("celsius")}
+        >
+          °C
+        </span>
+      </div>
+    );
+  } else {
+    return (
+      <div className="WeatherConversion">
+        <span className="temperature">
+          {Math.round(((props.temperature - 32) * 5) / 9)}
+        </span>
+        <span className="unit">°C</span>
+        <span className="unit"> | </span>
+        <span
+          className="unit convert"
+          onClick={() => props.setUnits("imperial")}
+        >
+          °F
+        </span>
+      </div>
+    );
+  }
+}
