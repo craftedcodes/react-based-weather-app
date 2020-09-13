b72f44a3bcdd7a464aaa66b61339f1c04eeacef8
miss6muffet <65518042+miss6muffet@users.noreply.github.com>
2020-09-13 19:11:28 +0200

    CSS

--- src/Weather.js
+++ src/Weather.js
@@ -1,23 +1,70 @@
-import React from "react";
-import WeatherIcon from "./WeatherIcon";
+import React, { useState } from "react";
+import WeatherInfo from "./WeatherInfo";
+import WeatherForecast from "./Forecast";
+import axios from "axios";
+import "./Weather.css";
 
 export default function Weather(props) {
-  let temp = Math.round(props.temperature);
-  let humid = Math.round(props.humidity);
-  let descript = props.description;
-  let wind = Math.round(props.wind);
-  let icon = WeatherIcon;
-  if (temp) {
+  const [weatherData, setWeatherData] = useState({ ready: false });
+  const [city, setCity] = useState(props.defaultCity);
+  const [units, setUnits] = useState("metric");
+
+  function handleResponse(response) {
+    setWeatherData({
+      ready: true,
+      temperature: response.data.main.temp,
+      humidity: response.data.main.humidity,
+      date: new Date(response.data.dt * 1000),
+      description: response.data.weather[0].description,
+      icon: response.data.weather[0].icon,
+      wind: response.data.wind.speed,
+      city: response.data.name,
+    });
+  }
+
+  function search() {
+    const apiKey = "1f29a8457a11c97a01e380819aae6d53";
+    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(handleResponse);
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    search();
+  }
+
+  function handleCityChange(event) {
+    setCity(event.target.value);
+  }
+
+  if (weatherData.ready) {
     return (
-      <div>
-        <p>Temperature: {temp}°C</p>
-        <p>Description: {descript}</p>
-        <p>Humidity: {humid}%</p>
-        <p>Wind: {wind}km/h</p>
-        <img src={icon} alt={descript} />
+      <div className="Weather">
+        <form onSubmit={handleSubmit}>
+          <div className="row">
+            <div className="col-9">
+              <input
+                type="search"
+                placeholder="Enter a city.."
+                className="form-control"
+                autoFocus="on"
+                onChange={handleCityChange}
+              />
+            </div>
+            <br />
+            <div className="col-3">
+              <input
+                type="submit"
+                value="Search"
+                className="btn btn-primary w-100"
+              />
+            </div>
+          </div>
+        </form>
       </div>
     );
   } else {
-    return "";
+    search();
+    return "Type a city";
   }
 }
