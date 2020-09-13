3e1bc09924fca42f430c327db74751e345a47957
miss6muffet <65518042+miss6muffet@users.noreply.github.com>
2020-09-13 15:06:11 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
   return (
     <div className="App">
       <Search />
-      <Weather />
+      <Weather defaultCity="Berlin" />
       <WeatherConversion />
       <br />
       <Forecast />
