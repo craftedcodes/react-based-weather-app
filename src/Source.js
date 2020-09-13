484245d5a793a1219c51b9666add2a129a877119
miss6muffet <65518042+miss6muffet@users.noreply.github.com>
2020-09-13 18:35:55 +0200

    CSS and more CSS

--- src/Source.js
+++ src/Source.js
@@ -4,11 +4,18 @@ import "./Source.css";
 export default function Source() {
   return (
     <div>
-      <a href="https://github.com/craftedcodes/react-based-weather-app">
-        Open source{" "}
-      </a>{" "}
-      <span> crafted by </span>{" "}
-      <a href="https://crafted-codes.netlify.app/">Lea Schumacher</a>
+      <a
+        href="https://github.com/craftedcodes/react-based-weather-app"
+        target="_blank"
+      >
+        Open source
+      </a>
+      <br />
+      <span> crafted by </span>
+      <br />
+      <a href="https://crafted-codes.netlify.app/" target="_blank">
+        Lea Schumacher
+      </a>
     </div>
   );
 }
--- src/WeatherIcon.js
+++ src/WeatherIcon.js
@@ -27,7 +27,7 @@ export default function WeatherIcon(props) {
     <ReactAnimatedWeather
       icon={codeMapping[props.code]}
       color="#2A354F"
-      size={52}
+      size={45}
       animate={true}
     />
   );
